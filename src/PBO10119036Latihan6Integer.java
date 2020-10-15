64abba2755fa1ae57f4e0b3db28c0f96cc7ec006
ALFI22-01 <72850530+ALFI22-01@users.noreply.github.com>
2020-10-15 12:59:39 +0700

    Project 7

---
+++ src/PBO10119036Latihan6Integer.java
@@ -0,0 +1,31 @@
+
+/**
+ *
+ * @author Alfi Nurizkya
+ * NAMA     : ALFI NURIZKYA
+ * KELAS    : PBO
+ * NIM      : 10119036
+ * DESKRIPSI PROGRAM : PERINTAH MEMASUKAN DATA INTEGER
+ */
+public class PBO10119036Latihan6Integer {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+      
+        byte b;
+        short s;
+        int i;
+        long l;
+        b = 120;
+        s = 32767;
+        i = 1_000_000_000;
+        l = 90000000000L;
+        System.out.println("byte  : " + b);
+        System.out.println("short : " + s);
+        System.out.println("int   : " + i);
+        System.out.println("long  : " + l);
+    }
+    
+}
